5fad4007d1586321abf3060bf98855efb658f377
CProgExpert <CProgExpert@users.noreply.github.com>
2017-09-14 14:29:23 -0400

    Work In Progress

---
+++ Help.h
@@ -0,0 +1,18 @@
+#ifndef HELP_H
+#define _HELP_H
+# include <string>
+using namespace std;
+
+class Assignment0
+	{
+	private:
+		 int  value = 0;
+		  string Characters = " ";
+	public:
+		void setValue (int);
+		int getValue ();
+		void setCharacters ();
+		int getCharacters ();
+	};
+#endif 
+
